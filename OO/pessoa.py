be642956941cab1f9ae58921c10a71df0a518663
Matheus <matheuscorrea12@gmai.com>
2020-08-29 22:39:13 -0300

    Criado atributo complexo filhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,15 +1,20 @@
 class Pessoa():
 
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35,):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Matheus')
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Luciano'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    julio = Pessoa(nome='Júlio', idade=25)
+    luciano = Pessoa(renzo, julio, nome='Luciano')
+    print(renzo.cumprimentar())
+    print(luciano.filhos)
+
+    for filho in luciano.filhos:
+        print(filho.nome)
